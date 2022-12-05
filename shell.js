bc27c48c95e0bdd61f0aac573ce6fee5fd197593
Mayank Kamboj <mayankkamboj47@gmail.com>
2022-12-05 17:52:44 +0530

    fix:Refactor
    1. Reduce globals in shell.js
    2. Distinguish specials and programs in preprocess()
    3. Shift pwd to pwd.js

--- shell.js
+++ shell.js
@@ -1,57 +1,53 @@
 /**
- * Bugs : 
- * EventEmitter memory leak. Are we forgetting to unassign memory somewhere ? 
- * To reproduce, use the terminal for a long time, and use node, cd back and forth.
- * May have cleared now, as I have taken care to clear the events I wasn't clearing earlier
- * 
- * executing a node script from some other directory should work as expected. We may need to change the
- * current directory in the process variable or something to get the effect.
- * 
- * add comments
- * Change code structure
  * 
  * ENOENT etc. should be communicated properly to the child process
  * 
+ * add fg and Ctrl + Z
+ * 
+ * bug : echo "hi there" will get called as ['echo', '"hi', 'there"']
  */
 
 const {spawn} = require('node:child_process');
 const fs = require('fs');
 const path = require('path');
 
+// Globals
+// Is there an alternative to using globals for communication ?
 let currentProcess;
-let curdir = process.cwd();
-const showShellPrompt = () => process.stdout.write(curdir + "> ");
+let curdir = process.cwd();         
+const prompt = () => process.stdout.write(curdir + ">>> ");
 
-let aliases = {
-    'ls' : (r)=>{
-        let ls = path.resolve(process.cwd(), 'ls');
-        return r.length > 1 ? ['node', ls, path.resolve(curdir,r[1])] : ['node', ls, curdir]
-    },
-    'node':(r)=>['node', path.resolve(curdir, r[1])]
-}
-let specials = {
-    'pwd': ()=>process.stdout.write(curdir),
-    'cd' : (r)=>changeDir((r.length > 1 && r[1]) || process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']),
-    'exit': ()=>process.exit(0)
-};
-showShellPrompt();
+// End of globals
+
+console.log(
+`
+Welcome to nodash ! Currently we support the following commands :
+ls, pwd, cd, exit, <path_to_node> args
+`
+);
+
+prompt();
 process.stdin.on('data', (r)=>{
-    if(currentProcess) return;
+    if(currentProcess) return;                        // ignore if some process is running already
     r = r.toString().split(/[ \t]/).map(r=>r.trim());
-    if(r.every(x=>x==='')) return showShellPrompt();
-    if(r[0] in specials){
-        specials[r[0]](r);
-        return showShellPrompt();
-    } 
-    if(r[0] in aliases) r = aliases[r[0]](r);
+    if(r.every(x=>x==='')) return prompt();
+    try {
+        r = preprocess(r);
+        console.log(r);
+    }
+    catch(e){
+        console.log(e);
+        return prompt();
+    }
+    if(!r) return prompt();
     currentProcess = createProcess(r);
+    // if something goes wrong, free currentProcess and display the error
     currentProcess.on('error', (e)=>{console.error(e); currentProcess = undefined;});
 });
 
-
 function createProcess([command, ...args]){
     let p = spawn(command, args, {cwd : curdir});
-    const killChild = ()=>p.kill('SIGINT') || p.kill('SIGKILL');
+    const killChild = ()=>p.kill('SIGINT') || p.kill('SIGKILL');    // try SIGINT before trying SIGKILL
     p.stdout.on('data', r=>process.stdout.write(r.toString()));
     p.stderr.on('data', r=>process.stderr.write(r.toString()));
     process.stdin.on('data', x=>p.stdin.write(x));
@@ -63,7 +59,7 @@ function createProcess([command, ...args]){
         process.stdin.off('data', p.stdin.write);
         process.off('SIGINT', killChild);
         process.removeAllListeners();
-        showShellPrompt();
+        prompt();                                      // yet another prompt
         currentProcess = undefined;
     });
     return p;
@@ -73,4 +69,30 @@ function changeDir(d){
     d = path.resolve(curdir, d);
     if(fs.lstatSync(path.resolve(d)).isDirectory()) curdir = d;
     else throw new Error(`Directory ${d} doesn't exist`);
+}
+
+function preprocess(s){
+    let specials = {
+        'cd' : (r)=>changeDir((r.length > 1 && r[1]) || process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']),
+        'exit': ()=>process.exit(0)
+    };    
+    let programs = {
+        'pwd' : [1, __dirname],              // add jsdoc for this object
+        'ls' : [1, '.'],                // command : [index of path argument, default path , ignore processing root]
+    };
+
+    if(s[0] in programs) {
+        let [pathIndex, pth, ignore] = programs[s[0]] || [];
+        if(!ignore) s[0] = path.resolve(__dirname, s[0]);
+        if(pathIndex!==undefined) s[pathIndex] = path.resolve(curdir, s[pathIndex] || pth);
+        s = ['node', ...s];
+    } else if(s[0] in specials){
+        specials[s[0]](s);
+        return;
+    }
+    else if(s[0]=='node') {
+        s[1] = path.resolve(curdir, s[1]);
+        return s;
+    }
+    return s;
 }
