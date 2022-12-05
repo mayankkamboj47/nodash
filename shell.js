72826cf5193427ad022aac1164136eab50da0725
Mayank Kamboj <mayankkamboj47@gmail.com>
2022-12-05 12:40:46 +0530

    fix:Execute files in pwd

--- shell.js
+++ shell.js
@@ -2,22 +2,34 @@
  * Bugs : 
  * EventEmitter memory leak. Are we forgetting to unassign memory somewhere ? 
  * To reproduce, use the terminal for a long time, and use node, cd back and forth.
+ * May have cleared now, as I have taken care to clear the events I wasn't clearing earlier
  * 
+ * executing a node script from some other directory should work as expected. We may need to change the
+ * current directory in the process variable or something to get the effect.
+ * 
+ * add comments
+ * Change code structure
+ * 
+ * ENOENT etc. should be communicated properly to the child process
  * 
  */
 
 const {spawn} = require('node:child_process');
 const fs = require('fs');
 const path = require('path');
-const showShellPrompt = () => process.stdout.write("> ");
 
 let currentProcess;
 let curdir = process.cwd();
+const showShellPrompt = () => process.stdout.write(curdir + "> ");
+
 let aliases = {
-    'ls' : (r)=>r.length > 1 ? ['node', 'ls', path.resolve(curdir,r[1])] : ['node', 'ls', curdir],
+    'ls' : (r)=>{
+        let ls = path.resolve(process.cwd(), 'ls');
+        return r.length > 1 ? ['node', ls, path.resolve(curdir,r[1])] : ['node', ls, curdir]
+    },
+    'node':(r)=>['node', path.resolve(curdir, r[1])]
 }
 let specials = {
-    '' : ,
     'pwd': ()=>process.stdout.write(curdir),
     'cd' : (r)=>changeDir((r.length > 1 && r[1]) || process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']),
     'exit': ()=>process.exit(0)
@@ -32,17 +44,16 @@ process.stdin.on('data', (r)=>{
         return showShellPrompt();
     } 
     if(r[0] in aliases) r = aliases[r[0]](r);
-    console.log(r);
     currentProcess = createProcess(r);
     currentProcess.on('error', (e)=>{console.error(e); currentProcess = undefined;});
 });
 
 
 function createProcess([command, ...args]){
-    let p = spawn(command, args);
+    let p = spawn(command, args, {cwd : curdir});
     const killChild = ()=>p.kill('SIGINT') || p.kill('SIGKILL');
     p.stdout.on('data', r=>process.stdout.write(r.toString()));
-    p.stderr.on('data', r=>process.stdout.write(r.toString()));
+    p.stderr.on('data', r=>process.stderr.write(r.toString()));
     process.stdin.on('data', x=>p.stdin.write(x));
 
     process.on('SIGINT', killChild);
@@ -59,7 +70,6 @@ function createProcess([command, ...args]){
 }
 
 function changeDir(d){
-    // note : d can be an absolute path, or a relative one. Make it absolute
     d = path.resolve(curdir, d);
     if(fs.lstatSync(path.resolve(d)).isDirectory()) curdir = d;
     else throw new Error(`Directory ${d} doesn't exist`);
